d0d0800943c33fd7d530ceb42ae30dc577a2f474
Jesse <jessexinyu@foxmail.com>
2024-09-05 06:23:31 +0000

    update

--- js/functions.js
+++ js/functions.js
@@ -37,24 +37,29 @@ $(window).resize(function() {
 	};
 })(jQuery);
 
+function calculateTimeDiff(date) {
+	const startDate = new Date(date); // 月份从0开始，所以要减去1
+	const endDate = new Date();
+	
+	const timeDiff = endDate.getTime() - startDate.getTime();
+	
+	const totalSeconds = Math.floor(timeDiff / 1000);
+	const days = Math.floor(totalSeconds / (24 * 60 * 60));
+	const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+	const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+	const seconds = totalSeconds % 60;
+	
+	return {
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds
+	};
+}
+
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
+	const { days, hours, minutes, seconds } = calculateTimeDiff(date)
+	console.log(days)
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
